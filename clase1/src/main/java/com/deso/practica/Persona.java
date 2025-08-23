33bcf75161047dd4a0dd073a55f2ec36b543d13a
elPeiretti <tomaspeiretti@gmail.com>
2025-08-23 18:36:28 -0300

    feat: add fields in Persona

--- clase1/src/main/java/com/deso/practica/Persona.java
+++ clase1/src/main/java/com/deso/practica/Persona.java
@@ -1,5 +1,31 @@
 package com.deso.practica;
 
 public class Persona {
-    
+    private String nombre;
+    private String apellido;
+    private Integer edad;
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public Integer getEdad() {
+        return edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public void setEdad(Integer edad) {
+        this.edad = edad;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 }
