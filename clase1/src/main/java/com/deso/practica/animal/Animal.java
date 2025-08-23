c8b370b42a583e9cf2d0035a484908ca828d1a83
elPeiretti <tomaspeiretti@gmail.com>
2025-08-23 18:34:26 -0300

    feat: add getters and setters in Animal

--- clase1/src/main/java/com/deso/practica/animal/Animal.java
+++ clase1/src/main/java/com/deso/practica/animal/Animal.java
@@ -7,4 +7,28 @@ public abstract class Animal {
     protected TipoAnimal tipo;
 
     protected abstract void hacerSonido();
+
+    public void setCantPatas(Integer cantPatas) {
+        this.cantPatas = cantPatas;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setTipo(TipoAnimal tipo) {
+        this.tipo = tipo;
+    }
+
+    public Integer getCantPatas() {
+        return cantPatas;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public TipoAnimal getTipo() {
+        return tipo;
+    }
 }
